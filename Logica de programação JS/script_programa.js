fc58c5fbd93d71e6f490c79c5e2d5ebe97fcef22
Thaynan <thaynan_ferreira@outlook.com.br>
2022-10-23 11:35:51 -0300

    Relembrando logica de programação com JS

---
+++ Logica de programação JS/script_programa.js
@@ -0,0 +1,13 @@
+var ano = 2022
+document.write("Thaynan tem " + (ano - 1997) + " anos");
+document.write("<br>");
+document.write("Thaynan tem " + (ano - 1800) + " anos");
+document.write("<br>");
+document.write("Thaynan tem " + (ano - 2000) + " anos");
+document.write("<br>");
+
+var idadeFlavio = 39;
+var idadeJoaquim = 20;
+var idadeBarney = 41;
+var media = (idadeFlavio + idadeBarney + idadeJoaquim)/3;
+document.write("a média das idades é " + Math.round(media))
