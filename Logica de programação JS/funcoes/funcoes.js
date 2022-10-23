8e13e923c78d60379b87c57977c3f4aeb3957d36
Thaynan <thaynan_ferreira@outlook.com.br>
2022-10-23 12:11:54 -0300

    Praticando funcoes

---
+++ Logica de programação JS/funcoes/funcoes.js
@@ -0,0 +1,23 @@
+var ano = 2022;
+function pulaLinha() {
+
+    document.write("<br>");
+
+}
+
+function mostra(texto) {
+
+    document.write(texto);
+
+}
+
+mostra("Thaynan tem " + (ano - 1997) + " anos");
+
+pulaLinha();
+
+mostra("Joaquim tem " + (ano - 1800) + " anos");
+
+pulaLinha();
+
+mostra("Bela tem " + (ano - 1998) + " anos");
+
