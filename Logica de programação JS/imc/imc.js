48094ea15c9dac1081349037969285dc5d6b608e
Thaynan <thaynan_ferreira@outlook.com.br>
2022-10-23 15:51:52 -0300

    Utilizando return de funções

--- Logica de programação JS/imc/imc.js
+++ Logica de programação JS/imc/imc.js
@@ -14,17 +14,21 @@ function escreva(frase) {
 function calculaImc(peso, altura) {
     
     imc = peso / (altura*altura);
-    escreva(`O IMC de quem tem ${altura}m de altura e ${peso}kg é ${Math.round(imc)}`);
+
+    return imc;
 
 }
 
 var peso = 73;
 var altura = 1.71;
-calculaImc(peso, altura);
+var imc1 = calculaImc(peso, altura);
+escreva(`O IMC de quem tem ${altura}m de altura e ${peso}kg é ${Math.round(imc1)}`);
 
-var peso2 = 100;
-var altura2 = 1.5;
-calculaImc(peso2, altura2);
+var peso2 = 68;
+var altura2 = 1.72;
+var imc2 = calculaImc(peso2, altura2);
+escreva(`O IMC de quem tem ${altura}m de altura e ${peso}kg é ${Math.round(imc2)}`);
+
 
 
 
