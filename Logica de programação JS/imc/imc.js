f968157fad8172ac4d4b351a6e76d4d363271f48
Thaynan <thaynan_ferreira@outlook.com.br>
2022-10-23 19:54:25 -0300

    recebendo informação do Usuario

--- Logica de programação JS/imc/imc.js
+++ Logica de programação JS/imc/imc.js
@@ -12,23 +12,22 @@ function escreva(frase) {
 }
 
 function calculaImc(peso, altura) {
-    
+        
     imc = peso / (altura*altura);
 
     return imc;
 
 }
 
-var peso = 73;
-var altura = 1.71;
-var imc1 = calculaImc(peso, altura);
-escreva(`O IMC de quem tem ${altura}m de altura e ${peso}kg é ${Math.round(imc1)}`);
+function clicar() {
+    var nomeInformado = document.querySelector("input.nome").value   
+    var alturaInformada = document.querySelector("input.altura").value;
+    var pesoInformado = document.querySelector("input.peso").value;
+    var resultado = document.querySelector("div.resposta");
+    var resposta = calculaImc(pesoInformado, alturaInformada)
+    resultado.innerHTML = `O IMC de ${nomeInformado} é ${resposta}`;
 
-var peso2 = 68;
-var altura2 = 1.72;
-var imc2 = calculaImc(peso2, altura2);
-escreva(`O IMC de quem tem ${altura}m de altura e ${peso}kg é ${Math.round(imc2)}`);
-
+}
 
 
 
