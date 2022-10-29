1d10a9f5763e58799da2e578ad3c7cf323ecc878
Thaynan <thaynan_ferreira@outlook.com.br>
2022-10-28 21:23:19 -0300

    Praticando

---
+++ Logica de programação JS/jogo/adivinha_mais.js
@@ -0,0 +1,12 @@
+function compare() {
+    var segredo = 5
+    var numPlayer = document.querySelector("input.numero").value
+
+    if (numPlayer == segredo) {
+        alert("Você acertou")
+    }
+    else{
+        alert("Você errou")
+    }
+    
+}
