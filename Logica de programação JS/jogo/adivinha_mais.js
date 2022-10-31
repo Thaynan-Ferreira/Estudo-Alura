ac09358000f0fe68963f112c6acb511ea1504cca
Thaynan <thaynan_ferreira@outlook.com.br>
2022-10-31 20:53:35 -0300

    Adição de array

--- Logica de programação JS/jogo/adivinha_mais.js
+++ Logica de programação JS/jogo/adivinha_mais.js
@@ -1,12 +1,19 @@
 function compare() {
-    var segredo = 5
+    var segredos = [5, 7, 10, 2];
     var numPlayer = document.querySelector("input.numero").value
 
-    if (numPlayer == segredo) {
-        alert("Você acertou")
+    var achou = false;
+    for (var contador = 0; contador < segredos.length; contador++) {
+        if (numPlayer == segredos[contador]) {
+            alert("Você acertou")
+            achou = true;
+            break;
+        }
     }
-    else{
+
+    if (achou == false) {
+        
         alert("Você errou")
+        
     }
-    
 }
