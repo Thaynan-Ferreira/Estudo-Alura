421555de92c4ea627ddf9ce55220abb1dabf922d
Thaynan <thaynan_ferreira@outlook.com.br>
2022-11-17 21:57:22 -0300

    criação de atributos privados utilizando a proposta da #

--- POO - JS/byteBank/index.js
+++ POO - JS/byteBank/index.js
@@ -1,6 +1,6 @@
 //CRIAÇÃO DE CLASSE
 class Cliente {
-    //IMPLEMENTAÇÃO DE ATRIBUTOS
+    //CRIAÇÃO DE ATRIBUTOS
     nome;
     CPF;
     conta;
@@ -9,18 +9,19 @@ class Cliente {
 
 class ContaCorrente {
     agencia;
-    saldo;
+    #saldo = 0;
 
     sacar(valor){
         //CRIAÇÃO DO METODO
-        if (valor <= this.saldo) {
-            this.saldo -= valor;// A PALAVRA this SERVE PARA QUE O TESTE SEJA FEITO NA DETERMINADA CLASSE QUE FOR CHAMADA 
+        if (valor <= this.#saldo) {
+            this.#saldo -= valor;// A PALAVRA this SERVE PARA QUE O TESTE SEJA FEITO NA DETERMINADA CLASSE QUE FOR CHAMADA 
         }
     }
 
     depositar(valor) {
         if (valor > 0 ) {
-            this.saldo += valor;
+            this.#saldo += valor;
+            console.log (this.#saldo)
         }
         
     }
@@ -36,17 +37,9 @@ cliente2.nome = "Isabela";
 cliente2.CPF = "88877766609";
 
 const contaCorrenteThaynan = new ContaCorrente();
-contaCorrenteThaynan.saldo = 0;
 contaCorrenteThaynan.agencia = 1001;
-console.log(contaCorrenteThaynan.saldo);
 
 contaCorrenteThaynan.depositar(1000)//CHAMANDO O METODO DE DEPOSITO
-
-console.log(contaCorrenteThaynan.saldo);
-
 contaCorrenteThaynan.sacar(500);//CHAMANDO O METODO
 
-console.log(contaCorrenteThaynan.saldo)
-
-console.log(cliente1);
-console.log(cliente2)
+console.log(contaCorrenteThaynan);
