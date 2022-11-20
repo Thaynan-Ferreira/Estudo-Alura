6e11648ecd793b1c11e745ad3912fe6c26f3965a
Thaynan <thaynan_ferreira@outlook.com.br>
2022-11-20 11:40:50 -0300

    Criando o primeiro componente react e tirando arquivos desnecessarios

---
+++ React/Organo/organograma/src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+function Banner() {
+    // JSX não é HTML
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="O banner principal da pagina Organo"/>
+        </header>
+        
+    )
+}
+
+export default Banner
