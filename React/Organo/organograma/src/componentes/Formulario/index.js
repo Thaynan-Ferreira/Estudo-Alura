459a5867fdbd6f2d63512919e02b099e47526db6
Thaynan <thaynan_ferreira@outlook.com.br>
2022-11-21 21:23:43 -0300

    Criação da lista de opções

--- React/Organo/organograma/src/componentes/Formulario/index.js
+++ React/Organo/organograma/src/componentes/Formulario/index.js
@@ -1,6 +1,17 @@
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 const Formulario = () => {
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'Ux e Design',
+        'Mobile',
+        'Inovação e Gestão'
+
+    ]
     return (
         <section className="formulario">
             <form>
@@ -8,6 +19,7 @@ const Formulario = () => {
             <CampoTexto label="Nome" placeholder="Digite seu Nome"/>
             <CampoTexto label="Cargo" placeholder="Digite seu Cargo"/>
             <CampoTexto label="Imagem" placeholder="Digite o endereço da Imagem"/>
+            <ListaSuspensa label="Time" itens={times}/>
             </form>
         </section>
     )
