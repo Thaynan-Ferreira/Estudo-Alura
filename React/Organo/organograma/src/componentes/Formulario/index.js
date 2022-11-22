8276c7e58f81d3f08d25d3ff21d7064deda158c2
Thaynan <thaynan_ferreira@outlook.com.br>
2022-11-22 06:36:12 -0300

    Adicioanndo função ao botao e fazendo com que ele tenha comportamento react

--- React/Organo/organograma/src/componentes/Formulario/index.js
+++ React/Organo/organograma/src/componentes/Formulario/index.js
@@ -11,16 +11,20 @@ const Formulario = () => {
         'Ux e Design',
         'Mobile',
         'Inovação e Gestão'
-
     ]
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('funcionou')
+    }
     return (
         <section className="formulario">
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-            <CampoTexto label="Nome" placeholder="Digite seu Nome"/>
-            <CampoTexto label="Cargo" placeholder="Digite seu Cargo"/>
-            <CampoTexto label="Imagem" placeholder="Digite o endereço da Imagem"/>
-            <ListaSuspensa label="Time" itens={times}/>
+            <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu Nome"/>
+            <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu Cargo"/>
+            <CampoTexto obrigatorio={true} label="Imagem" placeholder="Digite o endereço da Imagem"/>
+            <ListaSuspensa obrigatorio={true} label="Time" itens={times}/>
             <Botao>
                 Criar card
             </Botao>
