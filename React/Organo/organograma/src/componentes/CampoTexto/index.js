719e4cbee5dc79eb6bd75cae67ae936d0f4a2a0e
Thaynan <thaynan_ferreira@outlook.com.br>
2022-11-23 06:34:24 -0300

    Começando a pegar as informações digitadas.

--- React/Organo/organograma/src/componentes/CampoTexto/index.js
+++ React/Organo/organograma/src/componentes/CampoTexto/index.js
@@ -1,10 +1,19 @@
 import './CampoTexto.css'
 const CampoTexto = (props) => {
-    console.log()
+
+    let valor = ''
+
+    const aoDigitado = (evento) => {
+        valor = evento.target.value
+        console.log(valor)
+
+        
+    }
+
     return (
         <div className="campo__texto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={props.placeholder}></input>
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}></input>
         </div>
     )
 }
--- React/Organo/organograma/src/componentes/Formulario/index.js
+++ React/Organo/organograma/src/componentes/Formulario/index.js
@@ -20,7 +20,7 @@ const Formulario = () => {
     return (
         <section className="formulario">
             <form onSubmit={aoSalvar}>
-                <h2>Preencha os dados para criar o card do colaborador</h2>
+            <h2>Preencha os dados para criar o card do colaborador</h2>
             <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu Nome"/>
             <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu Cargo"/>
             <CampoTexto obrigatorio={true} label="Imagem" placeholder="Digite o endereço da Imagem"/>
