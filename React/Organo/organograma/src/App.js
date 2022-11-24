7ddb403e88c0ed5a93456e730e769354f9838e69
Thaynan <thaynan_ferreira@outlook.com.br>
2022-11-23 21:04:42 -0300

    Criando os Hooks para guardar os valores digitados/selecionados

--- React/Organo/organograma/src/App.js
+++ React/Organo/organograma/src/App.js
@@ -1,11 +1,19 @@
+import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
 
 function App() {
+
+  const [colaboradores, setColaboradores] = useState([])
+
+  const colaboradorCadastrado = (colaborador) => {
+    console.log(colaborador)
+  }
+
   return (
     <div className="App">
       <Banner />
-      <Formulario />    
+      <Formulario aoColaboradorCadastrado={colaborador => colaboradorCadastrado(colaborador)}/>    
     </div>
   );
 }
