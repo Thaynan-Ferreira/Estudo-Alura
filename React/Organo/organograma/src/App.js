856123df221c1731e2e4c2266b31dc756cd0745e
Thaynan <thaynan_ferreira@outlook.com.br>
2022-11-24 21:12:02 -0300

    Começando a implementação do resultado que sera exibido depois de preencher o formulario

--- React/Organo/organograma/src/App.js
+++ React/Organo/organograma/src/App.js
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
 function App() {
 
-  const [colaboradores, setColaboradores] = useState([])
+  // eslint-disable-next-line no-unused-vars
+  const [, setColaboradores] = useState([])
 
   const colaboradorCadastrado = (colaborador) => {
     console.log(colaborador)
@@ -13,7 +15,14 @@ function App() {
   return (
     <div className="App">
       <Banner />
-      <Formulario aoColaboradorCadastrado={colaborador => colaboradorCadastrado(colaborador)}/>    
+      <Formulario aoColaboradorCadastrado={colaborador => colaboradorCadastrado(colaborador)}/>
+      <Time nome="Programação"/> 
+      <Time nome="Front-end"/>   
+      <Time nome="Data Science"/>
+      <Time nome="Devops"/>
+      <Time nome="Ux e Design"/>
+      <Time nome="Mobile"/>
+      <Time nome="Inovação e Gestão"/>
     </div>
   );
 }
