afb339fc4031ae312a8f9dd9e5bc9f19cf6a2632
Thaynan <thaynan_ferreira@outlook.com.br>
2022-11-27 20:07:58 -0300

    Criação do dunb component do colaborador

---
+++ React/Organo/organograma/src/componentes/Colaborador/index.js
@@ -0,0 +1,20 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (
+        <div className='colaborador'>
+
+            <div className='cabecalho'>
+                <img src='https://github.com/Thaynan-Ferreira.png' alt='Thaynan Ferreira'/>
+            </div>
+
+            <div className='rodape'>
+                <h4>Thaynan Ferreira</h4>
+                <h5>Dev Font-end</h5>
+            </div>
+
+        </div>
+    )
+}
+
+export default Colaborador
