c973d10249728e4c883e7322d6443c7934b53853
Thaynan <thaynan_ferreira@outlook.com.br>
2022-12-15 23:00:30 -0300

    Iniciando a implementação do banner

--- Imersão React/aluratube/aluratube/pages/components/Header/index.js
+++ Imersão React/aluratube/aluratube/pages/components/Header/index.js
@@ -1,7 +1,22 @@
+import config from "../../../config.json";
+import styled from "styled-components";
 
+const StyledHeader = styled.div` 
+    img {
+        width: 80px;
+        height: 80px;
+        border-radius: 50%;
+    }
+`;
 const Header = () => {
     return (
-        <div>Header</div>
+        <StyledHeader>
+            <img src={`https://github.com/${config.github}.png`} alt="Banner" />
+            <img src="" alt="Foto do perfil" />
+            {config.name}
+            {config.job}
+
+        </StyledHeader>
     )
 }
 
--- Imersão React/aluratube/aluratube/pages/index.js
+++ Imersão React/aluratube/aluratube/pages/index.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Menu from "./components/Menu";
 import Timeline from "./components/Timeline";
+import styled from "styled-components";
 
 function Home() {
   return (
